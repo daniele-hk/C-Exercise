43f5ecb8ccae0e89892dc14d9c493b0c5513ccaa
daniele-hk <108331550+daniele-hk@users.noreply.github.com>
2022-08-15 09:16:59 +0200

    Add files via upload

---
+++ WPF - Learning List/LearningSkillList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF04_ListBox
+{
+    internal class LearningSkillList
+    {
+        public string SkillTitle { get; set; }
+        public int Completion { get; set; }
+        public int TimeSpent { get; set; }
+    }
+}
---
+++ WPF - Learning List/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF04_ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<LearningSkillList> skills = new List<LearningSkillList>();
+            skills.Add(new LearningSkillList() { SkillTitle = "HTML Basics", Completion = 100, TimeSpent = 10 });
+            skills.Add(new LearningSkillList() { SkillTitle = "CSS Basics", Completion = 100 });
+            skills.Add(new LearningSkillList() { SkillTitle = "JavaScript Basics", Completion = 50 });
+            skills.Add(new LearningSkillList() { SkillTitle = "Angular Basics", Completion = 100, TimeSpent = 147 });
+            skills.Add(new LearningSkillList() { SkillTitle = "C# Basics", Completion = 70, TimeSpent = 78 });
+
+            lbLearningList.ItemsSource = skills;
+        }
+
+        private void Show_Time_Detail(object sender, RoutedEventArgs e)
+        {
+            if(lbLearningList.SelectedItem != null)
+            {
+                MessageBox.Show("Hours of time spent learning: " + (lbLearningList.SelectedItem as LearningSkillList).TimeSpent);
+            }
+        }
+    }
+}
